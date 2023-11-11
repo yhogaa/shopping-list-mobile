64cabb284b62f4f8232fbc48358484a4c39341a5
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2023-11-11 22:22:02 +0700

    pindahin dikit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -74,11 +74,3 @@ class MyHomePage extends StatelessWidget  {
       );
     }
   }
-
-class ShopItem {
-    final String name;
-    final IconData icon;
-
-    ShopItem(this.name, this.icon);
-  }
-
--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 
+class ShopItem {
+    final String name;
+    final IconData icon;
+
+    ShopItem(this.name, this.icon);
+  }
+
 class ShopCard extends StatelessWidget {
   final ShopItem item;
 
